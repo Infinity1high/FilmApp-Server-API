38c8b30a4a62a22899163bc70c67aa522b7b6002
Dmitry Bondarenko <bondarenko.dmitry.85@gmail.com>
2018-04-02 18:10:41 +0300

    route fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
-const router = require('./router');
+const router = express.Router();
 const Film = require('./models/Films');
 const MONGOLAB_URI = 'mongodb://userTest:12345@ds231529.mlab.com:31529/films';
 
@@ -18,11 +18,17 @@ mongoose.connect(MONGOLAB_URI, (err, db) => {
 });
 
 
-
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*'}));
 const port = process.env.PORT || 8000;
 const  server = http.createServer(app);
+
+app.get('/', function(req,res, next) {
+    Film.find({}).then(item => {
+        res.json(item);
+});
+});
+
 
 server.listen(port);
 console.log('Server listen', port);
--- router.js
+++ router.js
@@ -1,9 +1,19 @@
 const Film = require('./models/Films');
+const router1 = express.Router();
+
+
 module.exports = function(app) {
-    app.get('/', function(req,res, next) {
+    router.get('/', function(req,res, next) {
+
         Film.find({}).then(item => {
             res.json(item);
         });
+    });
+    app.get('/api', function(req,res, next) {
+
+        Film.find({}).then(item => {
+            res.json(item);
+    });
     });
     app.post('/new_film', function(req, res) {
         Film.create({
