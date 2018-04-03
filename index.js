3bb947110da95149db24a25bf0d65e3fc45fa8e1
Dmitry Bondarenko <bondarenko.dmitry.85@gmail.com>
2018-04-03 14:12:08 +0300

    cors add

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
+const cors = require('cors');
 const Film = require('./models/Films');
 const MONGOLAB_URI = 'mongodb://userTest:12345@ds231529.mlab.com:31529/films';
 
@@ -19,6 +20,7 @@ mongoose.connect(MONGOLAB_URI, (err, db) => {
 
 
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*'}));
 const port = process.env.PORT || 8000;
 const  server = http.createServer(app);
@@ -31,4 +33,4 @@ app.get('/', function(req,res, next) {
 
 
 server.listen(port);
-console.log('Server listen', port);
+console.log('Server is up on port: ', port);
